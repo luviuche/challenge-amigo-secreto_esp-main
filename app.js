43fc1349a211db65243f4fd06c1ed2f2f901768c
Luis Viuche <lmvm.zzz@gmail.com>
2025-08-08 16:00:33 -0500

    Actualiza el archivo app.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-// Creacion de variables
-let amigos = [];
+// CREACION DE VARIABLES
+// Lista de amigos
+let listaDeAmigos = [];
+
+// FUNCIONES
+// Agregar amigo
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    // Condición para verificar si el campo está vacío
+    if (amigo === ""){
+        alert("Por favor, inserte un nombre");
+    } else {
+        // Agregar el amigo a la lista de amigos
+        listaDeAmigos.push(amigo);
+    }
+}
